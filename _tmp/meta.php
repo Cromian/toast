2db1a2db2575a742d6b8a43f68e010723ce24bae
Misha Vinokur <misha@Mishas-MacBook-Air-2.local>
2019-02-02 23:02:26 -0500

    intial commit

---
+++ _tmp/meta.php
@@ -0,0 +1,24 @@
+<meta charset="utf-8">
+<link rel="icon" type="image/x-icon" href="favicon.ico" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- <meta name="referrer" content="origin"> -->
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<!-- Twitter Card -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="<?php echo $site_name ?>">
+<meta name="twitter:description" content="<?php echo $site_description ?>">
+<meta name="twitter:image" content="_assets/og/twitter.png">
+
+<!-- Facebook Card -->
+<meta property="og:title" content="<?php echo $site_name ?>" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="<?php echo($site_url) ?>" />
+<meta property="og:image" content="_assets/og/facebook.png" />
+<meta property="og:description" content="<?php echo $site_description ?>" />
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+    $site_name = '';
+    $site_description = '';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?php echo $site_name ?></title>
+    <meta name="description" content="<?php echo $site_description ?>">
+    <?php include ('_tmp/meta.php'); ?>
+</head>
+<body>
+
+<header></header>
+<main></main>
+<footer></footer>
+
+</body>
+</head>
